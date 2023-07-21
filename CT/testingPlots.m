161f1c778ff74f556d1761a58748f638e2a29fb6
PENoble <pn399@bath.ac.uk>
2023-07-21 15:33:27 +0100

    New dataset and some edits

--- CT/testingPlots.m
+++ CT/testingPlots.m
@@ -3,7 +3,9 @@
 %Change seaice for whichever data you want
 % cd('Peter-Phoebe/CT')
 
-a = dir(fullfile(cd, 'data', 'ozone', '*.nc'));
+filepath = 'C:\Users\pn399\Documents\GitHub\Peter-Phoebe\CT\';
+
+a = dir(fullfile(filepath, 'data', 'seaice2017_1stofMonth', '*.nc'));
 n = numel(a);
 
 %Set up the actually interesting variable
@@ -12,29 +14,35 @@ n = numel(a);
 %7200 x 3600 for land temp (huge, only show one plot at a time if at all)
 %18 x 23 for ozone
 %Sea ice is best for animation, ozone is also cool though
-bigVar = zeros(18, 23, n);
+bigVar = zeros(432, 432, n);
 
 %Save interesting variable to array
 for bean = 1:n
     dataset = rCDF(fullfile(a(1).folder, a(bean).name), 0);
     %Change RHS for the variable you want
-    bigVar(:,:,bean) = dataset.merged_ozone_concentration;
+
+    bigVar(:,:,bean) = dataset.ice_conc;
 end
 
 %This is just bigshow
 %They already have their map set up as blank space?? Wonderful
-figure
-for x = 1:size(bigVar, 3)
+gcf = figure();
+set(gcf,'color','w','position',[50 50 700 500]);
 
-%         pcolor(bigVar(:,:,x));shading flat
-        %For sea level & land temp(need the ' for it to be the correct orientation)
-%         pcolor(bigVar(:,:,x)');shading flat
-%         clim([0 320])
-        %For ozone
-        pcolor(ozone.approximate_altitude, ozone.latitude_centers, bigVar(:,:,x));shading flat
-        colorbar
-        drawnow
-        pause(0.05)
+for ii = 1:50 % i.e. do this five times so the GIF repeats
+    for x = 1:size(bigVar, 3)
+    
+            pcolor(bigVar(:,:,x));shading flat
+            %For sea level & land temp(need the ' for it to be the correct orientation)
+    %         pcolor(bigVar(:,:,x)');shading flat
+    %         clim([0 320])
+            %For ozone
+            % pcolor(dataset.approximate_altitude, dataset.latitude_centers, bigVar(:,:,x));shading flat
+            colorbar
+            clim([0,100])
+            drawnow
+            pause(0.05)
+    end
 end
 
 %Just to make the land temp look nice
